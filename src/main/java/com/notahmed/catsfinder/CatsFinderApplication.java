f5d3672f5ae3a8c9bf3d1de236c07545826b1dc1
Ahmed Al-Thawadi <ahmed9althawadi@gmail.com>
2023-06-09 17:31:55 +0300

    adding project files

---
+++ src/main/java/com/notahmed/catsfinder/CatsFinderApplication.java
@@ -0,0 +1,13 @@
+package com.notahmed.catsfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatsFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatsFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/notahmed/catsfinder/CatsFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notahmed.catsfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatsFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
