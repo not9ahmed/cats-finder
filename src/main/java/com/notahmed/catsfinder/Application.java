e76e9e5a0c2c338be5b0b76e2e142f415b79596b
Ahmed Al-Thawadi <ahmed9althawadi@gmail.com>
2023-06-09 19:31:42 +0300

    Added files

--- src/main/java/com/notahmed/catsfinder/Application.java
+++ src/main/java/com/notahmed/catsfinder/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CatsFinderApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CatsFinderApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
