61ad9086ba009d381e9fa80d3f788ebbbaf60cea
Ahmed Al-Thawadi <ahmed9althawadi@gmail.com>
2023-09-05 21:21:15 +0300

    Trying to deploy

--- src/main/java/com/notahmed/catsfinder/config/DataLoader.java
+++ src/main/java/com/notahmed/catsfinder/config/DataLoader.java
@@ -1,9 +1,15 @@
 package com.notahmed.catsfinder.config;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.notahmed.catsfinder.model.User;
 import com.notahmed.catsfinder.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
+
+import java.io.InputStream;
+import java.util.List;
 //import org.springframework.stereotype.Controller;
 
 
@@ -12,15 +18,30 @@ import org.springframework.stereotype.Component;
 @Component
 public class DataLoader implements CommandLineRunner {
 
+    private final UserRepository userRepository;
+
+    // comes from jackson
+    // way to create objects out of json and deserialize to json
+    private final ObjectMapper objectMapper;
+
+    public DataLoader(UserRepository userRepository, ObjectMapper objectMapper) {
+        this.userRepository = userRepository;
+        this.objectMapper = objectMapper;
+    }
+
+
     @Override
     public void run(String... args) throws Exception {
 
-        // can save data here
-        final UserRepository userRepository;
-
-//        userRepository.saveAll()
 
         System.out.println("hello from data loader");
 
+//        try (InputStream inputStream = TypeReference.class.getResourceAsStream("/data/content.json")) {
+//
+//            userRepository.saveAll(objectMapper.readValue(inputStream, new TypeReference<List<User>>() {
+//            }));
+//
+//
+//        }
     }
 }
