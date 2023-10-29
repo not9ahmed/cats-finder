11a78869c76f2f8f356ce9c6803802ce280c474a
Ahmed Al-Thawadi <ahmed9althawadi@gmail.com>
2023-10-29 23:53:54 +0300

    day exercised completed

--- src/main/java/com/notahmed/catsfinder/security/JwtAuthenticationFilter.java
+++ src/main/java/com/notahmed/catsfinder/security/JwtAuthenticationFilter.java
@@ -20,5 +20,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
+        // TODO add auth required code
     }
 }
--- src/main/java/com/notahmed/catsfinder/security/SecurityConfiguration.java
+++ src/main/java/com/notahmed/catsfinder/security/SecurityConfiguration.java
@@ -1,14 +1,33 @@
 package com.notahmed.catsfinder.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration {
 
     // jwt service here
+    private final JwtService jwtService;
+
     // user details service here
+    // always add the service in the MyUserDetailsService so that spring security find its
+    private final MyUserDetailsService myuserDetailsService;
+
+    public SecurityConfiguration(JwtService jwtService, MyUserDetailsService myuserDetailsService) {
+        this.jwtService = jwtService;
+        this.myuserDetailsService = myuserDetailsService;
+    }
 
     // steps
     // 1- add the @EnableWebSecurity
@@ -19,6 +38,50 @@ public class SecurityConfiguration {
 
 
 
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+
+        daoAuthenticationProvider.setUserDetailsService(myuserDetailsService);
+
+        // gets from the bean below
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
 
 
+        return daoAuthenticationProvider;
+    }
+
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder () {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    // this used to register the authentication manager
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+
+
+
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        // add filter
+
+        http
+                .csrf(csrfConfigurer ->csrfConfigurer.disable())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                ).formLogin(Customizer.withDefaults());
+
+
+        return http.build();
+    }
 }
