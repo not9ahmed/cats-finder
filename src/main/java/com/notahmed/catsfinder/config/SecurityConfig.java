6f569126b9dcbba0507a941e13a4526a99380000
Ahmed Al-Thawadi <ahmed9althawadi@gmail.com>
2023-10-29 23:26:11 +0300

    using different auth way

--- src/main/java/com/notahmed/catsfinder/config/SecurityConfig.java
+++ src/main/java/com/notahmed/catsfinder/config/SecurityConfig.java
@@ -1,11 +1,11 @@
 package com.notahmed.catsfinder.config;
 
-import com.nimbusds.jose.jwk.JWK;
-import com.nimbusds.jose.jwk.JWKSet;
-import com.nimbusds.jose.jwk.RSAKey;
-import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
-import com.nimbusds.jose.jwk.source.JWKSource;
-import com.nimbusds.jose.proc.SecurityContext;
+//import com.nimbusds.jose.jwk.JWK;
+//import com.nimbusds.jose.jwk.JWKSet;
+//import com.nimbusds.jose.jwk.RSAKey;
+//import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+//import com.nimbusds.jose.jwk.source.JWKSource;
+//import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,10 +13,10 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.JwtEncoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+//import org.springframework.security.oauth2.jwt.JwtDecoder;
+//import org.springframework.security.oauth2.jwt.JwtEncoder;
+//import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+//import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -65,15 +65,15 @@ public class SecurityConfig {
         //oauth2ResourceServer will use jwt and jwt requires decoder in order to work
 
         return http
-                .csrf(csrf -> csrf.disable())
-                .authorizeRequests(
-                        auth -> auth.anyRequest().authenticated()
-                )
-                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
-                .sessionManagement(
-                        session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                )
-                .httpBasic(withDefaults())
+//                .csrf(csrf -> csrf.disable())
+//                .authorizeRequests(
+//                        auth -> auth.anyRequest().authenticated()
+//                )
+//                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+//                .sessionManagement(
+//                        session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                )
+//                .httpBasic(withDefaults())
                 .build();
     }
 
@@ -83,22 +83,22 @@ public class SecurityConfig {
      * @return
      */
 
-    @Bean
-    JwtDecoder jwtDecoder(){
-        return NimbusJwtDecoder.withPublicKey(rsaKeys.publicKey()).build();
-    }
+//    @Bean
+//    JwtDecoder jwtDecoder(){
+//        return NimbusJwtDecoder.withPublicKey(rsaKeys.publicKey()).build();
+//    }
 
 
     /**
      * Bean to create encoded jwt then use it inside authentication controller
      * @return encoded jwt
      */
-    @Bean
-    JwtEncoder jwtEncoder(){
-        JWK jwk = new RSAKey.Builder(rsaKeys.publicKey()).privateKey(rsaKeys.privateKey()).build();
-
-        JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
-
-        return new NimbusJwtEncoder(jwks);
-    }
+//    @Bean
+//    JwtEncoder jwtEncoder(){
+//        JWK jwk = new RSAKey.Builder(rsaKeys.publicKey()).privateKey(rsaKeys.privateKey()).build();
+//
+//        JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+//
+//        return new NimbusJwtEncoder(jwks);
+//    }
 }
