98f5a5fd5c52d8ba3c364bcb913a7569947e3c0a
Ahmed Al-Thawadi <ahmed9althawadi@gmail.com>
2023-11-22 23:54:17 +0300

    added user stories and erd changes

--- README.md
+++ README.md
@@ -5,6 +5,28 @@ The aim of the project is two develop a simple cats finder system.
 The users will be able to post their cats and share cute pics of their cats.
 
 
+## User Stories
+* The user can create a new account with username, password, and add additional details such email, name, etc.
+* User must sign in to the system to receive a token to indicate his authentication, and authorization.
+* User can edit his details and update the necessary details.
+* The user can delete his account from the system.
+* 
+* User can add his cats with their name, description, images and choose the breed, and also the system can help in identifying the cat breed (Machine Learning).
+* User can update and delete his cats from the system.
+* User must have the ability to add some comments about their cat.
+* 
+
+* 
+* The admin user can create new cat breeds with the necessary information such as breed name, origin country, fun facts and images.
+* The admin user can update and delete breeds from the system when required.
+* Admin user can delete users from the system and disable them.
+* 
+*
+* The user can create a new post with their cat with new and full content with images.
+* Users can also update their cats' post and have the option to delete.
+* Other user can browse through the system and find posts of cat breeds which they like.
+*
+
 
 ## Requirements
 * The system should store the info about the users.
--- src/main/java/com/notahmed/catsfinder/breed/Breed.java
+++ src/main/java/com/notahmed/catsfinder/breed/Breed.java
@@ -55,7 +55,9 @@ public class Breed {
         private String name;
         private String description;
         private ArrayList<String> images;
-        private String originCountry;
+        private String country;
+
+        // long text
         private String history;
 
         // array list or mappedCollection
@@ -63,109 +65,7 @@ public class Breed {
         private ArrayList<String> funFacts;
 
 
-        public Breed(Long id, String name, String description, ArrayList<String> images, String originCountry, String history, ArrayList<String> characteristics, ArrayList<String> funFacts) {
-                this.id = id;
-                this.name = name;
-                this.description = description;
-                this.images = images;
-                this.originCountry = originCountry;
-                this.history = history;
-                this.characteristics = characteristics;
-                this.funFacts = funFacts;
-        }
 
-
-        public Long getId() {
-                return id;
-        }
-
-        public void setId(Long id) {
-                this.id = id;
-        }
-
-        public String getName() {
-                return name;
-        }
-
-        public void setName(String name) {
-                this.name = name;
-        }
-
-        public String getDescription() {
-                return description;
-        }
-
-        public void setDescription(String description) {
-                this.description = description;
-        }
-
-        public ArrayList<String> getImages() {
-                return images;
-        }
-
-        public void setImages(ArrayList<String> images) {
-                this.images = images;
-        }
-
-        public String getOriginCountry() {
-                return originCountry;
-        }
-
-        public void setOriginCountry(String originCountry) {
-                this.originCountry = originCountry;
-        }
-
-        public String getHistory() {
-                return history;
-        }
-
-        public void setHistory(String history) {
-                this.history = history;
-        }
-
-        public ArrayList<String> getCharacteristics() {
-                return characteristics;
-        }
-
-        public void setCharacteristics(ArrayList<String> characteristics) {
-                this.characteristics = characteristics;
-        }
-
-        public ArrayList<String> getFunFacts() {
-                return funFacts;
-        }
-
-        public void setFunFacts(ArrayList<String> funFacts) {
-                this.funFacts = funFacts;
-        }
-
-
-        @Override
-        public boolean equals(Object o) {
-                if (this == o) return true;
-                if (o == null || getClass() != o.getClass()) return false;
-                Breed breed = (Breed) o;
-                return Objects.equals(id, breed.id) && Objects.equals(name, breed.name) && Objects.equals(description, breed.description) && Objects.equals(images, breed.images) && Objects.equals(originCountry, breed.originCountry) && Objects.equals(history, breed.history) && Objects.equals(characteristics, breed.characteristics) && Objects.equals(funFacts, breed.funFacts);
-        }
-
-        @Override
-        public int hashCode() {
-                return Objects.hash(id, name, description, images, originCountry, history, characteristics, funFacts);
-        }
-
-        @Override
-        public String toString() {
-                return "Breed{" +
-                        "id=" + id +
-                        ", name='" + name + '\'' +
-                        ", description='" + description + '\'' +
-                        ", images=" + images +
-                        ", originCountry='" + originCountry + '\'' +
-                        ", history='" + history + '\'' +
-                        ", characteristics=" + characteristics +
-                        ", funFacts=" + funFacts +
-                        '}';
-        }
 }
 
  */
